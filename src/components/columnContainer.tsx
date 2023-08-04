66e85c0fbef386a339266b4da806e929b90ef925
Islam Kamel <dev.islam.kamel@gmail.com>
2023-08-05 00:37:34 +0300

    fix: Add gap between tasks

--- src/components/columnContainer.tsx
+++ src/components/columnContainer.tsx
@@ -169,7 +169,7 @@ function ColumnContainer({ column, onDelete, onUpdate, tasks, createTask, onDele
         </button>
       </div>
       <div className="flex flex-grow flex-col p-2 overflow-y-auto gap-2">
-        <div className="h-[100%] overflow-x-auto">
+        <div className="h-[100%] overflow-x-auto flex flex-col gap-2">
           <SortableContext items={tasksId}>
             {tasks.map(task => (
               <TaskCard task={task} key={task.id} onDeleteTask={onDeleteTask} onTaskUpdate={onTaskUpdate} />
@@ -196,6 +196,7 @@ function ColumnContainer({ column, onDelete, onUpdate, tasks, createTask, onDele
           <PlusCircle />
           Add Task
         </button>
+
       </div>
     </div>
   );
